373d78b33137ada237e8a0a40e0e6c3bfb0eefbd
cashanblack <cashan@wp.pl>
2017-03-23 17:20:13 +0100

    Update scripts.js
    Poprawione (mam nadzieję)..

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
-var triangle1Area, triangle2Area, triangle3Area;
+var triangle1Area;
+var triangle2Area;
+var triangle3Area;
 		
 function getTriangleArea(a, h) {
-	if (a<0 || h<0){return "Nieprawidłowe dane";}
-	return a*h/2;
+	if (a < 0 || h < 0){
+		return "Nieprawidłowe dane";
+	}
+	return a * h / 2;
 }
 		
 triangle1Area = getTriangleArea(10,6);
 triangle2Area = getTriangleArea(2,1);
 triangle3Area = getTriangleArea(4,5);
 		
-console.log( triangle1Area );
-console.log( triangle2Area );
-console.log( triangle3Area );
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
