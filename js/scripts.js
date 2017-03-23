0e93a85c56f2e6d351fb8520a5db23ea189cd229
cashanblack <cashan@wp.pl>
2017-03-23 08:36:39 +0100

    Improved formatting
    Wprowadziłem proprawki formatowania .

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-var a=0;
-var h=0;
-var triangleArea=0;
+var a = 0;
+var h = 0;
+var triangleArea = 0;
 
-a=prompt ("Podaj długośc podstawy trójkąta");
-h=prompt ("Podaj wysokość trójkąta");
+a = prompt("Podaj długośc podstawy trójkąta");
+h = prompt("Podaj wysokość trójkąta");
 
-triangleArea=a*h/2;
+triangleArea = a * h / 2;
 
-console.log ('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 		
