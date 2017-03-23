813d294bab9b7659c8de22c7b576279185c33251
cashan <cashan@wp.pl>
2017-03-23 17:08:53 +0100

    ex4 update

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,16 @@
-var a, b, value, switcher;
+var a;
+var b; 
+var value;
 
-a=prompt ("Podaj a:");
-b=prompt ("Podaj b:");
+a = prompt("Podaj a:");
+b = prompt("Podaj b:");
 			
-value = (a*a) + (2*a*b) - (b*b);
+value = ( a * a ) + ( 2 * a * b ) - ( b * b );
 		
-console.log ('Wynik operacji wynosi: ' + value);
-/*
-if (value>0){ 
-	console.log ('Wynik dodatni');
-}
-		
-if (value<0){
-	console.log ('Wynik ujemny');
-}
+console.log('Wynik operacji wynosi: ' + value);
 
-if (value==0){
-	console.log ('Wynik równy zero');
-}
-*/
-/*
-if (value>0){ 
-	console.log ('Wynik dodatni');
-} else if (value<0){
-	console.log ('Wynik ujemny');
-} else if (value==0){
-	console.log ('Wynik równy zero');
-}
-
-*/
-
-switcher = (value<0)*(-1) + (value==0)*(0) + (value>0)*(1);
-switch (switcher) { 
-	case -1: 
-		console.log ('Wynik ujemny'); 
-		break; 
-	case 0: 
-		console.log ('Wynik równy zero');
-		break;
-	case 1: 
-		console.log ('Wynik dodatni'); 
-		break; 
-	default: 
-		alert("Trudno wyczaić..."); 
+if(value > 0){ 
+	console.log('Wynik dodatni');
+} else {
+	value < 0 ? console.log ('Wynik ujemny') : console.log ('Wynik równy zero');
 }
